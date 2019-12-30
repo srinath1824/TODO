78b1e069f7a4bec157bb743e070d63ed066b8cdf
srinath puppala <srinath.p@zen3.com>
2019-12-30 20:52:56 +0530

    button change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import TodoList from "./components/Todolist/todoList";
+import { Button } from "@material-ui/core";
 import TodoWithRedux from "./components/containers/todoContainer";
 
 class App extends Component {
@@ -8,9 +9,30 @@ class App extends Component {
     super(props);
   }
 
+  handleClick() {
+    window.open(
+      "https://www.google.com",
+      "_blank",
+      "location=yes,scrollbars=yes,status=yes"
+    );
+  }
+
   render() {
     // return <TodoList />;
-    return <TodoWithRedux />;
+    return (
+      <React.Fragment>
+        <TodoWithRedux />
+        <div style={{ textAlign: "center", padding: "10px" }}>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => this.handleClick()}
+          >
+            Open Google
+          </Button>
+        </div>
+      </React.Fragment>
+    );
   }
 }
 
